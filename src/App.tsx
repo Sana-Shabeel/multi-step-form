ee21b3d42e2509400c9402aa8f823aac4cdd7688
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-05 08:17:47 +0300

    Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import PersonalInfo from "./components/PersonalInfo";
+import SelectPlan from "./components/SelectPlan";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="h-screen bg-magnolia">
+      <header className="bg-sidebar-mobile w-full h-[172px] flex justify-center gap-4 pt-6">
+        {[1, 2, 3, 4].map((num) => (
+          <div
+            className="w-9 h-9 rounded-full bg-transparent grid place-content-center border text-white"
+            key={num}
+          >
+            {num}
+          </div>
+        ))}
+      </header>
+
+      <SelectPlan />
+    </main>
+  );
 }
 
-export default App
+export default App;
