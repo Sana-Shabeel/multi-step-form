1a81495d127e994ee553deae51b943cc60120b74
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-05 08:19:42 +0300

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "sidebar-mobile": "url('/src/assets/images/bg-sidebar-mobile.svg')",
+        "sidebar-desktop": "url('/assets/images/bg-sidebar-desktop.svg')",
+      },
+      colors: {
+        coolGray: "hsl(231, 11%, 63%)",
+        lightGray: " hsl(229, 24%, 87%)",
+        magnolia: "hsl(217, 100%, 97%)",
+        alabaster: "hsl(231, 100%, 99%)",
+        marineBlue: "hsl(213, 96%, 18%)",
+        publishBlue: "hsl(243, 100%, 62%)",
+        pasteBlue: "hsl(228, 100%, 84%)",
+        lightBlue: "hsl(206, 94%, 87%)",
+        strawberryBlue: "hsl(354, 84%, 57%)",
+      },
+    },
+  },
+  plugins: [],
+};
