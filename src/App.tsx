917bad5781dc39bb5baaf44e53ce696a41293af9
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-08 14:31:11 +0300

    Add Add-ons

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import SelectPlan from "./components/SelectPlan";
+import { useState } from "react";
+import AddOns from "./components/AddOns";
 
 function App() {
+  const [enabled, setEnabled] = useState(false);
+
   return (
     <main className="h-screen bg-magnolia">
-      <header className="bg-sidebar-mobile w-full h-[172px] flex justify-center gap-4 pt-6">
+      <header className="bg-sidebar-mobile bg-cover bg-center w-full h-[172px] flex justify-center gap-4 pt-6 md:gap-10">
         {[1, 2, 3, 4].map((num) => (
           <div
-            className="w-9 h-9 rounded-full bg-transparent grid place-content-center border text-white"
+            className="w-9 h-9 rounded-full bg-transparent grid place-content-center border text-white "
             key={num}
           >
             {num}
           </div>
         ))}
       </header>
-
-      <SelectPlan />
+      <AddOns enabled={enabled} />
     </main>
   );
 }
