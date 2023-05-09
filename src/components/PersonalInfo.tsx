f47d8fe5f8c04f229f3b242320134a284c2f3611
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-09 08:08:19 +0300

    Add personal comp xl screen

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -2,7 +2,7 @@ import Headline from "./Headline";
 
 const PersonalInfo = () => {
   return (
-    <div className="w-[343px] -mt-20 mx-auto bg-white py-6 px-4 rounded-md grid grid-cols-1 gap-6">
+    <div className="w-[343px] sm:w-[70%] lg:w-[50%] -mt-20 mx-auto bg-white py-6 px-4 rounded-md grid grid-cols-1 gap-6 xl:mx-0 xl:m-12 xl:w-[450px] xl:p-0 xl:h-96">
       <Headline
         title="Personal Info"
         description="Please provide your name, email address, and phone number."
@@ -15,7 +15,7 @@ const PersonalInfo = () => {
           type="text"
           placeholder="e.g. John Doe"
           id="name"
-          className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm "
+          className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm xl:py-4"
         />
       </div>
       <div>
@@ -26,7 +26,7 @@ const PersonalInfo = () => {
           type="text"
           placeholder="e.g. johndoe@lorem.com"
           id="name"
-          className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm "
+          className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm xl:py-4"
         />
       </div>
       <div className="mb-4">
@@ -37,7 +37,7 @@ const PersonalInfo = () => {
           type="text"
           placeholder="e.g. +1 234 567 890"
           id="name"
-          className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm "
+          className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm xl:py-4"
         />
       </div>
     </div>
