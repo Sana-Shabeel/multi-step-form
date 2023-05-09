c75ec8006a4d341f948669e83120d99ed8e0fcbc
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-09 08:07:54 +0300

    Add headline xl screen

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -6,8 +6,8 @@ interface Props {
 const Headline = ({ title, description }: Props) => {
   return (
     <div className="my-2">
-      <h1 className="text-2xl font-bold">{title}</h1>
-      <p className="text-coolGray mt-1">{description}</p>
+      <h1 className="text-2xl font-bold xl:text-3xl">{title}</h1>
+      <p className="text-coolGray mt-1 xl:mt-3">{description}</p>
     </div>
   );
 };
