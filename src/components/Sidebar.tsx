48da81434dcc33daeabc09facf2c3f67f93137f7
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-09 08:08:50 +0300

    Add sidebar comp xl screen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,7 +23,28 @@ const Sidebar = () => {
       description: "SUMMARY",
     },
   ];
-  return <div className="bg-sidebar-mobile xl:bg-sidebar-desktop"></div>;
+  return (
+    <aside className="hidden w-[274px] xl:block bg-white h-[568px] bg-sidebar-desktop bg-cover bg-center">
+      <div className="px-8 py-8">
+        {steps.map((step) => (
+          <div className="mb-6 flex items-center gap-4" key={step.id}>
+            <div
+              className="w-8 h-8 rounded-full bg-transparent grid place-content-center border text-white "
+              key={step.id}
+            >
+              {step.id}
+            </div>
+            <div>
+              <p className="text-sm text-coolGray">
+                {step.title.toUpperCase()}
+              </p>
+              <h3 className="text-md text-white mb-1">{step.description}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
