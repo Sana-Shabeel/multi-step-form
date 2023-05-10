f88f22a4952c0a8cb4846107ff859935b360cdac
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-10 08:15:43 +0300

    Add useMutltistepForm hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
-const Header = () => {
+interface HeaderProps {
+  step: number;
+}
+
+const Header = ({ step }: HeaderProps) => {
+  console.log(step);
+
   return (
     <header className="bg-sidebar-mobile bg-cover bg-center xl:hidden w-full h-[172px] flex justify-center gap-4 pt-6 md:gap-10">
       {[1, 2, 3, 4].map((num) => (
         <div
-          className="w-9 h-9 rounded-full bg-transparent grid place-content-center border text-white "
+          className={`${
+            step + 1 === num ? "bg-lightGray text-black" : ""
+          } w-9 h-9 rounded-full bg-transparent grid place-content-center border text-white `}
           key={num}
         >
           {num}
