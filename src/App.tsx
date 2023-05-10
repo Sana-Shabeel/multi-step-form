e53b4da664acdac583bf63bd787edfcef1ebfd9c
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-10 08:15:31 +0300

    Add useMutltistepForm hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import ThankYou from "./components/ThankYou";
+import { useMultistepForm } from "./hooks/useMultistepForm";
+import PersonalInfo from "./components/PersonalInfo";
+import SelectPlan from "./components/SelectPlan/SelectPlan";
+import AddOns from "./components/Add-ons/AddOns";
+import FinishingUp from "./components/FinishingUp";
 
 function App() {
   const [enabled, setEnabled] = useState(false);
 
+  const { step, steps, nextStep, prevStep } = useMultistepForm([
+    <PersonalInfo />,
+    <SelectPlan enabled={false} setEnabled={setEnabled} />,
+    <AddOns enabled={enabled} />,
+    <FinishingUp />,
+    <ThankYou />,
+  ]);
+  console.log(step);
+
   return (
     <main className="h-screen bg-magnolia xl:grid place-items-center">
       <div className="xl:hidden">
-        <Header />
-        <ThankYou />
-        <Footer />
+        <Header step={step} />
+        {steps[step]}
+        <Footer next={nextStep} back={prevStep} step={step} />
       </div>
 
       {/* at 1440px */}
       <div className="hidden w-[940px] mx-auto p-4 rounded-md h-[600px] bg-white xl:flex gap-20">
-        <Sidebar />
+        <Sidebar step={step} />
         <div>
-          <ThankYou />
-          <Footer />
+          {steps[step]}
+          <Footer next={nextStep} back={prevStep} step={step} />
         </div>
       </div>
     </main>
