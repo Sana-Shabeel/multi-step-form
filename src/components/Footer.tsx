0efc58f301ed39d145d436f6b0416c0073636e9d
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-11 12:18:24 +0300

    Remove log

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,15 @@ interface FooterProps {
   next: () => void;
   back: () => void;
   step: number;
+  // handleSumbit: UseFormHandleSubmit<FieldValues>;
+  // onSubmit: (data: object) => void;
 }
 
 const Footer = ({ next, back, step }: FooterProps) => {
   return (
     <div className="h-[72px] bg-white flex justify-between items-center absolute bottom-0 w-full px-4 xl:relative xl:px-0">
       <button
+        type="button"
         className={`${
           step === 0 || step === 4 ? "invisible" : ""
         } bg-transparent text-coolGray px-4 py-2 border-none hover:text-marineBlue `}
@@ -16,10 +19,9 @@ const Footer = ({ next, back, step }: FooterProps) => {
         Go Back
       </button>
       <button
-        className={`${step === 4 ? "hidden" : ""}
-          
-          ${step === 3 ? "bg-publishBlue hover:bg-pasteBlue" : ""}
-          
+        type="submit"
+        className={`${step === 4 ? "hidden" : ""}         
+          ${step === 3 ? "bg-publishBlue hover:bg-pasteBlue" : ""}  
           bg-marineBlue px-4 py-2 text-white rounded-md hover:bg-[#174987]`}
         onClick={next}
       >
