53502942be9861eb456c5c1d7fde864cd140cf0a
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-11 12:22:28 +0300

    Add Context Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,47 @@
-import { SetStateAction, useState } from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import ThankYou from "./components/ThankYou";
-import { useMultistepForm } from "./hooks/useMultistepForm";
-import PersonalInfo from "./components/PersonalInfo";
-import SelectPlan from "./components/SelectPlan/SelectPlan";
+import { useState } from "react";
 import AddOns from "./components/Add-ons/AddOns";
 import FinishingUp from "./components/FinishingUp";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import PersonalInfo from "./components/PersonalInfo";
+import SelectPlan from "./components/SelectPlan/SelectPlan";
+import Sidebar from "./components/Sidebar";
+import ThankYou from "./components/ThankYou";
+import { FormDataProvider } from "./context/FormDataContext";
+import { useMultistepForm } from "./hooks/useMultistepForm";
 
 function App() {
   const [enabled, setEnabled] = useState(false);
 
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // console.log("Form submitted!", e);
+  };
+
   const { step, steps, nextStep, prevStep } = useMultistepForm([
     <PersonalInfo />,
-    <SelectPlan enabled={false} setEnabled={setEnabled} />,
+    <SelectPlan enabled={enabled} setEnabled={setEnabled} />,
     <AddOns enabled={enabled} />,
     <FinishingUp />,
     <ThankYou />,
   ]);
-  console.log(step);
 
   return (
     <main className="h-screen bg-magnolia xl:grid place-items-center">
-      <div className="xl:hidden">
-        <Header step={step} />
-        {steps[step]}
-        <Footer next={nextStep} back={prevStep} step={step} />
-      </div>
-
-      {/* at 1440px */}
-      <div className="hidden w-[940px] mx-auto p-4 rounded-md h-[600px] bg-white xl:flex gap-20">
-        <Sidebar step={step} />
-        <div>
-          {steps[step]}
-          <Footer next={nextStep} back={prevStep} step={step} />
-        </div>
-      </div>
+      <FormDataProvider>
+        <form
+          onSubmit={onSubmit}
+          className="xl:w-[940px] xl:mx-auto xl:p-4 xl:rounded-md xl:h-[600px] xl:bg-white xl:flex gap-20"
+        >
+          <Header step={step} />
+          <Sidebar step={step} />
+          <div>
+            {steps[step]}
+            <Footer next={nextStep} back={prevStep} step={step} />
+          </div>
+        </form>
+      </FormDataProvider>
     </main>
   );
 }
