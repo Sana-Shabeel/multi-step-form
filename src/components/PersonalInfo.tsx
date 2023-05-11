6baf8b7f4cea08344860531be814e45abc93a0d8
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-11 12:20:31 +0300

    Add collect formData using context api

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
+import { FormDataContext } from "../context/FormDataContext";
 import Headline from "./Headline";
 
 const PersonalInfo = () => {
+  const { updateFormData } = useContext(FormDataContext);
+
+  const handleChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
+    updateFormData(e.target.name, e.target.value);
+  };
   return (
     <div className="w-[343px] sm:w-[70%] lg:w-[50%] -mt-20 mx-auto bg-white py-6 px-4 rounded-md grid grid-cols-1 gap-6 xl:mx-0 xl:m-12 xl:w-[450px] xl:p-0 xl:h-96">
       <Headline
@@ -15,28 +22,34 @@ const PersonalInfo = () => {
           type="text"
           placeholder="e.g. John Doe"
           id="name"
+          name="name"
+          onChange={handleChanges}
           className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm xl:py-4"
         />
       </div>
       <div>
-        <label htmlFor="name" className="text-gray-700 block text-sm mb-1">
+        <label htmlFor="email" className="text-gray-700 block text-sm mb-1">
           Email Address
         </label>
         <input
           type="text"
           placeholder="e.g. johndoe@lorem.com"
-          id="name"
+          id="email"
+          name="email"
+          onChange={handleChanges}
           className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm xl:py-4"
         />
       </div>
       <div className="mb-4">
-        <label htmlFor="name" className="text-gray-700 block text-sm mb-1">
+        <label htmlFor="number" className="text-gray-700 block text-sm mb-1">
           Phone Number
         </label>
         <input
-          type="text"
+          type="number"
           placeholder="e.g. +1 234 567 890"
-          id="name"
+          id="number"
+          name="number"
+          onChange={handleChanges}
           className="border-gray-300 block w-full rounded-md border py-2 pl-3 shadow-sm outline-none focus:border-b-4 focus:border-marineBlue sm:text-sm xl:py-4"
         />
       </div>
