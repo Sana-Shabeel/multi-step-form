6a5903fa3e029d99cb061fd2972eb945ec49c27c
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-11 12:18:31 +0300

    Remove log

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,6 @@ interface HeaderProps {
 }
 
 const Header = ({ step }: HeaderProps) => {
-  console.log(step);
-
   return (
     <header className="bg-sidebar-mobile bg-cover bg-center xl:hidden w-full h-[172px] flex justify-center gap-4 pt-6 md:gap-10">
       {[1, 2, 3, 4].map((num) => (
