2b8f254743857228f8e45de9ffee2f36a80d7b70
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-15 13:52:10 +0300

    Add submit form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AddOns from "./components/Add-ons/AddOns";
 import FinishingUp from "./components/FinishingUp";
 import Footer from "./components/Footer";
@@ -7,17 +7,12 @@ import PersonalInfo from "./components/PersonalInfo";
 import SelectPlan from "./components/SelectPlan/SelectPlan";
 import Sidebar from "./components/Sidebar";
 import ThankYou from "./components/ThankYou";
-import { FormDataProvider } from "./context/FormDataContext";
+import { FormDataContext } from "./context/FormDataContext";
 import { useMultistepForm } from "./hooks/useMultistepForm";
 
 function App() {
   const [enabled, setEnabled] = useState(false);
-
-  const onSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    // console.log("Form submitted!", e);
-  };
+  const { formData, validateFormData } = useContext(FormDataContext);
 
   const { step, steps, nextStep, prevStep } = useMultistepForm([
     <PersonalInfo />,
@@ -27,21 +22,29 @@ function App() {
     <ThankYou />,
   ]);
 
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log(formData, formData);
+
+    if (validateFormData()) {
+      nextStep();
+    }
+  };
+
   return (
     <main className="h-screen bg-magnolia xl:grid place-items-center">
-      <FormDataProvider>
-        <form
-          onSubmit={onSubmit}
-          className="xl:w-[940px] xl:mx-auto xl:p-4 xl:rounded-md xl:h-[600px] xl:bg-white xl:flex gap-20"
-        >
-          <Header step={step} />
-          <Sidebar step={step} />
-          <div>
-            {steps[step]}
-            <Footer next={nextStep} back={prevStep} step={step} />
-          </div>
-        </form>
-      </FormDataProvider>
+      <form
+        onSubmit={onSubmit}
+        className="xl:w-[940px] xl:mx-auto xl:p-4 xl:rounded-md xl:h-[600px] xl:bg-white xl:flex gap-20"
+      >
+        <Header step={step} />
+        <Sidebar step={step} />
+        <div>
+          {steps[step]}
+          <Footer back={prevStep} step={step} />
+        </div>
+      </form>
     </main>
   );
 }
