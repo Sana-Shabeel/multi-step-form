93a8e27c74242565ea660771f08da94d858136dc
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-15 13:55:30 +0300

    Add validation

--- src/context/FormDataContext.tsx
+++ src/context/FormDataContext.tsx
@@ -1,22 +1,48 @@
 import { createContext, useCallback, useState } from "react";
+import { z } from "zod";
 
-interface FormData {
-  [key: string]: string | number | boolean | undefined | object;
-}
+const addOns = z.object({
+  title: z.string(),
+  price: z.number(),
+  status: z.boolean(),
+});
+
+const formDataSchema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  phone: z.string().min(6),
+  plan: z.string(),
+  Online: addOns.optional(),
+  Larger: addOns.optional(),
+  Customizable: addOns.optional(),
+});
+
+type FormData = z.infer<typeof formDataSchema>;
+
+const INITIAL_FORM_DATA: FormData = {
+  name: "",
+  email: "",
+  phone: "",
+  plan: "",
+};
 
 interface FormDataContextProps {
   formData: FormData;
+  validationErrors: { [key: string]: string };
   updateFormData: (
     key: string,
     value: string | number | boolean | undefined | object
   ) => void;
+  validateFormData: () => boolean;
 }
 
 export const FormDataContext = createContext<FormDataContextProps>({
-  formData: {},
+  formData: INITIAL_FORM_DATA,
+  validationErrors: {},
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   updateFormData: () => {},
   // eslint-disable-next-line @typescript-eslint/no-empty-function
+  validateFormData: () => false,
 });
 
 interface FormDataProviderProps {
@@ -24,20 +50,52 @@ interface FormDataProviderProps {
 }
 
 export const FormDataProvider = ({ children }: FormDataProviderProps) => {
-  const [formData, setFormData] = useState<FormData>({});
+  const [formData, setFormData] = useState<FormData>(INITIAL_FORM_DATA);
+  const [validationErrors, setValidationErrors] = useState<{
+    [key: string]: string;
+  }>({});
 
   //This ensures that the function is only created once and memoized as long as the setFormData function does not change.
   const updateFormData = useCallback(
     (key: string, value: string | number | boolean | undefined | object) => {
-      setFormData((prevData) => ({ ...prevData, [key]: value }));
+      setFormData((prevData) => {
+        const newData = { ...prevData, [key]: value };
+        return newData;
+      });
     },
     [setFormData]
   );
 
-  console.log(formData);
+  const validateFormData = (): boolean => {
+    try {
+      formDataSchema.parse(formData); // validate the new form data
+      setValidationErrors({});
+      console.log("validateFormData", "Form is valid");
+      return true;
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const errors: { [key: string]: string } = {};
+        error.errors.forEach((err) => {
+          if (err.path) {
+            const field = err.path[0];
+            const message = err.message;
+            errors[field] = message;
+          }
+        });
+        setValidationErrors(errors);
+        console.log("validationErrors", errors);
+        return false;
+      } else {
+        console.log("Unknown error!", error);
+        throw error;
+      }
+    }
+  };
 
   return (
-    <FormDataContext.Provider value={{ formData, updateFormData }}>
+    <FormDataContext.Provider
+      value={{ formData, updateFormData, validateFormData, validationErrors }}
+    >
       {children}
     </FormDataContext.Provider>
   );
