07a674d1c6dd01821ef83d3c909c4744d4489445
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-15 13:52:58 +0300

    Hide back btn

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,9 @@
 interface FooterProps {
-  next: () => void;
   back: () => void;
   step: number;
-  // handleSumbit: UseFormHandleSubmit<FieldValues>;
-  // onSubmit: (data: object) => void;
 }
 
-const Footer = ({ next, back, step }: FooterProps) => {
+const Footer = ({ back, step }: FooterProps) => {
   return (
     <div className="h-[72px] bg-white flex justify-between items-center absolute bottom-0 w-full px-4 xl:relative xl:px-0">
       <button
@@ -23,7 +20,6 @@ const Footer = ({ next, back, step }: FooterProps) => {
         className={`${step === 4 ? "hidden" : ""}         
           ${step === 3 ? "bg-publishBlue hover:bg-pasteBlue" : ""}  
           bg-marineBlue px-4 py-2 text-white rounded-md hover:bg-[#174987]`}
-        onClick={next}
       >
         {step === 3 ? "Confirm" : "Next Step"}
       </button>
