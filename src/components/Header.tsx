97619eeae08b36f82cbb21d4dc0cd65242bb1026
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-05-15 13:54:24 +0300

    Add header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,17 @@ interface HeaderProps {
 }
 
 const Header = ({ step }: HeaderProps) => {
+  console.log("step", step + 1);
+
   return (
     <header className="bg-sidebar-mobile bg-cover bg-center xl:hidden w-full h-[172px] flex justify-center gap-4 pt-6 md:gap-10">
       {[1, 2, 3, 4].map((num) => (
         <div
           className={`${
-            step + 1 === num ? "bg-lightGray text-black" : ""
-          } w-9 h-9 rounded-full bg-transparent grid place-content-center border text-white `}
+            step + 1 === num
+              ? "bg-lightGray text-black"
+              : "bg-transparent text-white"
+          } w-9 h-9 rounded-full  grid place-content-center border  `}
           key={num}
         >
           {num}
